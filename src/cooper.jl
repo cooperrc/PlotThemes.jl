fb9ca64a72548e979ad7084eb3a2afc458a2f58e
Ryan C Cooper <cooperrc@users.noreply.github.com>
2022-07-22 18:36:20 -0400

    updating style to match changes in #67
    Changes made in this PR:
    
    https://github.com/JuliaPlots/PlotThemes.jl/pull/67

--- src/cooper.jl
+++ src/cooper.jl
@@ -1,14 +1,15 @@
-_themes[:cooper] = PlotTheme(linewidth = 4,
-                             markersize = 10,
-                             fontfamily = "sans-serif",
-                             titlefontsize = 24,
-                             guidefontsize = 18,
-                             tickfontsize = 18,
-                             colorbar_tickfontsize = 18,
-                             legend_font_pointsize = 18,
-                             legend=:outertopright,
+const _cooper = PlotTheme(Dict([linewidth = 4,
+                             :markersize = 10,
+                             :fontfamily = "sans-serif",
+                             :titlefontsize = 24,
+                             :guidefontsize = 18,
+                             :tickfontsize = 18,
+                             :colorbar_tickfontsize = 18,
+                             :legend_font_pointsize = 18,
+                             :legend=:outertopright,
 #                             legend_font = Plots.Font("sans-serif", 
 #                                                      18, :hcenter, :vcenter, 
 #                                                      0.0, RGB{N0f8}(0.0,0.0,0.0)),
-                             grid = false
-                            )
+                             :grid = false
+                            ])
+)
